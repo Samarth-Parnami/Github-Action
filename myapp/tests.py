ac10cb0d3245f311aa86356a8e0331ae3e322437
Samarth-Parnami <samarth.p@beaconstac.com>
2023-06-26 14:29:59 +0530

    updated tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
-from .views import covered_function
+from .views import covered_function, added_new_function
 # Create your tests here.
 
 class CoveringCoveredFunctionTest(TestCase):
     def test_covered_function(self):
         covered_function()
+
+    def test_added_new_function(self):
+        added_new_function()
