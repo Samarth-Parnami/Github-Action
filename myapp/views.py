7df72945718a8b4535771a156f6fc3b004e6e068
Samarth-Parnami <samarth.p@beaconstac.com>
2023-06-26 13:52:57 +0530

    A very important New feature

--- myapp/views.py
+++ myapp/views.py
@@ -27,3 +27,10 @@ def another_function_that_is_uncovered():
     print("adding print statemets to add lines to the function")
     print("adding print statemets to add lines to the function")
     print("adding print statemets to add lines to the function")
+
+
+def added_new_function():
+    print("something")
+    print("something")
+    print("something")
+    print("something")
