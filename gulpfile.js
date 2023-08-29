abc59b8c738aa9f9602cb23680e46757d35c5071
amulya <amulya.k@beaconstac.com>
2023-08-29 15:03:41 +0530

    sd

--- gulpfile.js
+++ gulpfile.js
@@ -105,8 +105,8 @@ function getGitChangedImages() {
 
     const modifiedOutput = execSync('git diff --name-only --diff-filter=A HEAD^').toString();
     const untrackedOutput = execSync('git ls-files --others --exclude-standard').toString();
-    const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-    const diffOutput = execSync(`git diff ${currentBranch}..develop`).toString();
+    // const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+    const diffOutput = execSync(`git diff ${currentBranch}..develop --diff-filter=A --name-only`).toString();
     console.log(diffOutput);
     console.log(diffOutput.split('\n'));
     const output = modifiedOutput + untrackedOutput.split('\n').map(line => 'A\t' + line).join('\n');
