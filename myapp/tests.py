88176b26fd8d4001e5bf2d78dede1fbc6fb7177d
Samarth-Parnami <samarth.p@beaconstac.com>
2023-09-08 13:35:21 +0530

    cleanup

--- myapp/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .views import covered_function
-# Create your tests here.
-
-class CoveringCoveredFunctionTest(TestCase):
-    def test_covered_function(self):
-        covered_function()
